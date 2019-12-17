9e9973ae7db7ad01ef5ce4fa87d52acf9af72e89
perci <perci@Gavin-Laptop.local>
2019-12-17 09:22:14 -0800

    modified wapo to format better

--- chrome-extension/clear-wapo.js
+++ chrome-extension/clear-wapo.js
@@ -32,33 +32,53 @@ function runEmbedded() {
 	}
     }
     for ( ; i < adds.length; i++ ) {
-	   if ( adds[i].type == "text" ) {
-           var div_node = document.createElement("DIV");
-	       var text_node = document.createElement("P");
-	       text_node.innerHTML = adds[i].content;
+	if ( adds[i].type == "text" ) {
+            var div_node = document.createElement("DIV");
+	    var text_node = document.createElement("P");
+	    text_node.innerHTML = adds[i].content;
 //    	    var the_text = document.createTextNode(adds[i].content);
 //    	    text_node.appendChild(the_text);
-	       text_node.classList.add( "font--body", "font-copy",  "color-gray-darkest", "ma-0", "pad-bottom-md",  "undefined" );
-	       div_node.appendChild(text_node);
-	       t.appendChild(div_node);
-	   }
-       else if ( adds[i].type == "image" ) {
-           var div_node = document.createElement("DIV");
-           var fig_node = document.createElement("FIGURE");
-	       var img_node = document.createElement("IMG");
-           var fig_caption_node = document.createElement("FIGCAPTION");
-           fig_node.classList.add("center", "mb-md", "ml-neg-gutter", "mr-neg-gutter", "ml-auto-ns", "mr-auto-ns", "hide-for-print");
-           img_node.src = adds[i].resized_urls.large;
-           img_node.alt = adds[i].caption;
-           img_node.width = adds[i].width;
-           img_node.classList.add("mw-100");
-           fig_node.appendChild(img_node);
-           fig_caption_node.classList.add("left", "ml-gutter", "mr-gutter", "mr-auto-ns", "ml-auto-ns", "gray-dark", "font--subhead", "font-xxxs", "mt-xs", "mb-sm");
-           fig_caption_node.innerHTML = adds[i].caption;
-           fig_node.appendChild(fig_caption_node);
-           div_node.appendChild(fig_node);
-           t.appendChild(div_node);
-       }
+	    text_node.classList.add( "font--body", "font-copy",  "color-gray-darkest", "ma-0", "pad-bottom-md",  "undefined" );
+	    div_node.appendChild(text_node);
+	    t.appendChild(div_node);
+	}
+	else if ( adds[i].type == "raw_html" ) {
+            var div_node = document.createElement("DIV");
+	    var html_node = document.createElement("DIV");
+	    html_node.innerHTML = adds[i].content;
+	    div_node.appendChild(html_node);
+	    t.appendChild(div_node);
+	}
+	else if ( adds[i].type == "image" ) {
+            var div_node = document.createElement("DIV");
+            var fig_node = document.createElement("FIGURE");
+	    var img_node = document.createElement("IMG");
+            var fig_caption_node = document.createElement("FIGCAPTION");
+            fig_node.classList.add("center", "mb-md", "ml-neg-gutter", "mr-neg-gutter", "ml-auto-ns", "mr-auto-ns", "hide-for-print");
+            img_node.src = adds[i].resized_urls.large;
+            img_node.alt = adds[i].caption;
+            img_node.width = adds[i].width;
+            img_node.classList.add("mw-100");
+            fig_node.appendChild(img_node);
+            fig_caption_node.classList.add("left", "ml-gutter", "mr-gutter", "mr-auto-ns", "ml-auto-ns", "gray-dark", "font--subhead", "font-xxxs", "mt-xs", "mb-sm");
+            fig_caption_node.innerHTML = adds[i].caption;
+            fig_node.appendChild(fig_caption_node);
+            div_node.appendChild(fig_node);
+            t.appendChild(div_node);
+	}
+	else if ( adds[i].type == "video" ) {
+	}
+	else if ( adds[i].type == "header" ) {
+            var div_node = document.createElement("DIV");
+	    var text_node = document.createElement("H3");
+	    text_node.innerHTML = adds[i].content;
+//    	    var the_text = document.createTextNode(adds[i].content);
+//    	    text_node.appendChild(the_text);
+	    text_node.classList.add( "font--body", "font-copy",  "color-gray-darkest", "ma-0", "pad-bottom-md",  "undefined" );
+	    div_node.appendChild(text_node);
+	    t.appendChild(div_node);
+	}
+		
     }
     var oUrl = document.querySelector("meta[property='og:url']").getAttribute("content");
     window.history.pushState("","", oUrl);
