73be5cc57c973c0c07a343a00fc982853c538080
perci <ptieng@gmail.com>
2020-09-27 09:48:59 -0700

    added bloomberg.com; clean up popup.js

--- src/firefox-extension/popup.js
+++ src/firefox-extension/popup.js
@@ -6,49 +6,53 @@
 			    var url = new URL( tabs[0].url );
 			    console.log( url.hostname );
 			    switch (url.hostname) {
-				case "www.smdailyjournal.com":
+				case "www.bloomberg.com":
 			            chrome.tabs.executeScript( tabs[0].id,
-							   { file: "clear-smjournal.js"} );
-			            break;
- 			        case "www.nytimes.com":
-			            chrome.tabs.executeScript( tabs[0].id,
-							   { file: "clear-nytimes.js"} );
-			            break;
-			        case "www.foreignpolicy.com":
-			        case "foreignpolicy.com":
-			            chrome.tabs.executeScript( tabs[0].id,
-							   { file: "clear-foreignpolicy.js"} );
- 			        case "www.theatlantic.com":
-			            chrome.tabs.executeScript( tabs[0].id,
-							   { file: "clear-theatlantic.js"} );
-			            break;
- 			        case "www.theguardian.com":
-			            chrome.tabs.executeScript( tabs[0].id,
-							   { file: "clear-theguardian.js"} );
-			            break;
- 			        case "www.thedailybeast.com":
-			            chrome.tabs.executeScript( tabs[0].id,
-							   { file: "clear-thedailybeast.js"} );
-			            break;
-				case "www.latimes.com":
-			            chrome.tabs.executeScript( tabs[0].id,
-							   { file: "clear-latimes.js"} );
-			            break;
-				case "www.sandiegouniontribune.com":
-			            chrome.tabs.executeScript( tabs[0].id,
-							   { file: "clear-latimes.js"} );
+							   { file: "clear-bloomberg.js"} );
 			            break;
 				case "www.chicagotribune.com":
 			            chrome.tabs.executeScript( tabs[0].id,
 							   { file: "clear-latimes.js"} );
 			            break;
-				case "www.washingtonpost.com":
+			        case "www.foreignpolicy.com":
+			        case "foreignpolicy.com":
 			            chrome.tabs.executeScript( tabs[0].id,
-							   { file: "clear-wapo.js"} );
+							   { file: "clear-foreignpolicy.js"} );
+				case "www.latimes.com":
+			            chrome.tabs.executeScript( tabs[0].id,
+							   { file: "clear-latimes.js"} );
 			            break;
 				case "www.mediaite.com":
 			            chrome.tabs.executeScript( tabs[0].id,
 							   { file: "clear-mediaite.js"} );
+			            break;
+ 			        case "www.nytimes.com":
+			            chrome.tabs.executeScript( tabs[0].id,
+							   { file: "clear-nytimes.js"} );
+			            break;
+				case "www.sandiegouniontribune.com":
+			            chrome.tabs.executeScript( tabs[0].id,
+							   { file: "clear-latimes.js"} );
+			            break;
+				case "www.smdailyjournal.com":
+			            chrome.tabs.executeScript( tabs[0].id,
+							   { file: "clear-smjournal.js"} );
+			            break;
+ 			        case "www.theatlantic.com":
+			            chrome.tabs.executeScript( tabs[0].id,
+							   { file: "clear-theatlantic.js"} );
+			            break;
+ 			        case "www.thedailybeast.com":
+			            chrome.tabs.executeScript( tabs[0].id,
+							   { file: "clear-thedailybeast.js"} );
+			            break;
+ 			        case "www.theguardian.com":
+			            chrome.tabs.executeScript( tabs[0].id,
+							   { file: "clear-theguardian.js"} );
+			            break;
+				case "www.washingtonpost.com":
+			            chrome.tabs.executeScript( tabs[0].id,
+							   { file: "clear-wapo.js"} );
 			            break;
 			        default:
 				    console.log( "unrecognized : ", tabs[0].url );
