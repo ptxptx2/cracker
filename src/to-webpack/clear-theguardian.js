2ddf6f0bf733c0715da6f6f1f96ffcf89f5a2197
perci <ptieng@gmail.com>
2022-12-12 07:42:39 -0800

    maintenance

--- src/to-webpack/clear-theguardian.js
+++ src/to-webpack/clear-theguardian.js
@@ -1,21 +1,42 @@
+import * as helpers from '../common/helpers.js';
+
 // remove sign-in-gate
 
 // v1
+if ( !helpers.removeElementById("sign-in-gate") ) {
+}    
+
+/* 
 d = document.getElementById("sign-in-gate");
 if ( d != null ) {
     d.parentNode.removeChild(d);
 }
+*/
 
 // v2
+if ( !helpers.removeElementsByClassName("signin-gate") ) {
+}    
+
+/* 
 d = document.getElementsByClassName("signin-gate");
 if ( d != null && d.length > 0) {
     d[0].parentNode.removeChild(d[0]);
 }
+*/
 
+
+if ( !helpers.setStyleByClassName("js-article__body", "display", "block") ) {
+}    
+/*
 d = document.getElementsByClassName("js-article__body");
 if ( d != null && d.length > 0 ) {
     d[0].style.display = "block";
 }
+*/
+
+// add removal of money thing - class css-34t5a0
+if ( !helpers.removeElementsByClassName("css-34t5a0") ) {
+}    
 
 /* 
 d = document.getElementsByClassName("js-article__body");
@@ -33,4 +54,3 @@ if ( d != null && d.length > 0 ) {
 
 
 
-// fix if error on any step
