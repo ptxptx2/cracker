34ab516586089a851ba01c01b998fdd4de4d90b1
perci <ptieng@gmail.com>
2023-07-07 05:26:05 -0700

    modified chicago tribune

---
+++ src/to-webpack/clear-chicagotribune.js
@@ -0,0 +1,5 @@
+import * as helpers from '../common/helpers.js';
+
+helpers.removeFirstElementByClassName(" fEy1Z2XT  ");
+helpers.setStyleByClassName( "HTML", "overflow", "scroll" );
+helpers.setStyleByClassName( "BODY", "overflow", "scroll" );
