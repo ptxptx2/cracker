fb0bc7c47cee3f2492d268f0e39b6d892db106a6
perci <ptieng@gmail.com>
2025-01-19 06:00:03 -0800

    added clear-cnbc.js

---
+++ src/to-webpack/clear-cnbc.js
@@ -0,0 +1,12 @@
+import * as helpers from '../common/helpers.js';
+
+// delete fEy1Z2XT div class
+helpers.removeFirstElementByClassName("fEy1Z2XT");
+
+var d;
+
+d = document.getElementsByTagName("BODY");
+d[0].style.overflow = "scroll";
+
+d = document.getElementsByTagName("HTML");
+d[0].style.overflow = "scroll";
