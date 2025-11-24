9f0994dd85ba53849aa185cb4564582c80d6cf1d
perci <ptieng@gmail.com>
2025-11-23 22:03:28 -0800

    modified eater

--- src/to-webpack/clear-eater.js
+++ src/to-webpack/clear-eater.js
@@ -3,8 +3,8 @@ import * as helpers from '../common/helpers.js';
 // remove popup div
 helpers.removeFirstElementByClassName( " fEy1Z2XT  " );
 
-helpers.setStyleByTagName( "HTML", "overflow", "scroll" );
-helpers.setStyleByTagName( "BODY", "overflow", "scroll" );
+helpers.setStyleByTagName( "HTML", overflow, "scroll" );
+helpers.setStyleByTagName( "BODY", overflow, "scroll" );
 
 
 
